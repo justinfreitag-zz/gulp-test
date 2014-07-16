cc77dd07a3a8c5838b7ff5729e2b7639248ca844
Justin Freitag <justin.freitag@gmail.com>
2014-07-17 05:12:24 +1000

    Added complexity test

--- index.js
+++ index.js
@@ -1,13 +1,10 @@
 'use strict';
 
-var help = require('gulp-help');
-var istanbul = require('gulp-istanbul');
-var istanbulEnforcer = require('gulp-istanbul-enforcer');
-var jshint = require('gulp-jshint');
+var eventStream = require('event-stream');
+var plugins = require('gulp-load-plugins')({
+  config: __dirname + '/package.json'
+});
 var merge = require('merge');
-var mocha = require('gulp-mocha');
-var exit = require('gulp-exit');
-var rimraf = require('gulp-rimraf');
 var stylish = require('jshint-stylish');
 var through = require('through2');
 
@@ -29,9 +26,9 @@ var DEFAULT_OPTIONS = {
     test: 'test/**/*.js'
   },
   complexity: {
-    trycatch: true,
-    halstead: [10, 13, 20],
-    maintainability: 90
+    cyclomatic: [3, 7, 12],
+    halstead: [8, 13, 20],
+    maintainability: 100
   },
   coverage: {
     thresholds: {
@@ -63,33 +60,38 @@ function end(done) {
 module.exports = function (gulp, options) {
   gulp.options = merge(DEFAULT_OPTIONS, options);
 
-  help(gulp);
+  plugins.help(gulp);
 
-  gulp.task('clean', 'Clean target folder.', function () {
-    return gulp
-      .src('target', {read: false})
-      .pipe(rimraf({force: true}));
+  gulp.task('add-lint-files', 'Add lint files.', function (done) {
+    var jshintPaths = ['**/*.jshintrc', '!node_modules/**/*'];
+    eventStream.concat(
+      gulp
+        .src(jshintPaths, {dot: true})
+        .pipe(plugins.debug())
+        .pipe(plugins.rename(function (path) {
+          path.extname = '.old';
+        }))
+        .pipe(gulp.dest(process.cwd())),
+      gulp
+        .src(jshintPaths, {dot: true, cwd: __dirname})
+        .pipe(plugins.debug())
+        .pipe(gulp.dest(process.cwd()))
+    ).on('end', done);
   });
 
-  gulp.task('copy-lint-files', 'Copy lint files.', function () {
-    return gulp
-      .src(__dirname + '/**/*.jshintrc')
-      .pipe(gulp.dest('tmp'));
-  });
-
-  gulp.task('test', 'Perform tests only.', function (done) {
+  gulp.task('test', 'Perform unit tests only.', function (done) {
     gulp
       .src(gulp.options.paths.test, {read: false})
-      .pipe(mocha(gulp.options.test))
+      .pipe(plugins.mocha(gulp.options.test))
       .pipe(end(done))
-      .pipe(exit());
+      .pipe(plugins.exit());
   });
 
   gulp.task('lint', 'Perform lint tests.', function () {
     return gulp
       .src(gulp.options.paths.lint, {read: false})
-      .pipe(jshint())
-      .pipe(jshint.reporter(stylish));
+      .pipe(plugins.jshint())
+      .pipe(plugins.jshint.reporter(stylish));
   });
 
   gulp.options.coverage = merge({
@@ -100,29 +102,29 @@ module.exports = function (gulp, options) {
   gulp.task('coverage', 'Perform coverage tests.', function (done) {
     gulp
       .src(gulp.options.paths.coverage.src)
-      .pipe(istanbul())
+      .pipe(plugins.istanbul())
       .on('finish', function () {
         var coverageError;
         gulp.src(gulp.options.paths.test)
-          .pipe(mocha(gulp.options.test))
-          .pipe(istanbul.writeReports({
+          .pipe(plugins.mocha(gulp.options.test))
+          .pipe(plugins.istanbul.writeReports({
             dir: gulp.options.paths.coverage.dest,
             reporters: ['json', 'lcov']
           }))
-          .pipe(istanbulEnforcer(gulp.options.coverage))
+          .pipe(plugins.istanbulEnforcer(gulp.options.coverage))
           .on('error', function (error) {
             coverageError = error;
           })
           .on('end', function () {
             done(coverageError);
           })
-          .pipe(exit());
+          .pipe(plugins.exit());
       });
   });
 
   gulp.task('complexity', 'Perform complexity tests.', function () {
-    //return gulp.src(gulp.options.paths.coverage.src)
-    //  .pipe(plato(gulp.options.paths.complexity));
+    return gulp.src(gulp.options.paths.lint)
+      .pipe(plugins.complexity(gulp.options.complexity));
   });
 
 };
